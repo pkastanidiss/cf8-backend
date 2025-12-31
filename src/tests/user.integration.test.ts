51cd82da215074aaa528382fd39988e450738000
pkastanidiss <pkastanidiss@gmail.com>
2025-12-31 03:17:22 +0200

    Jest test

---
+++ src/tests/user.integration.test.ts
@@ -0,0 +1,45 @@
+import { TestServer } from "./testSetup";
+import userRoutes from '../routes/user.routes';
+import User from '../models/user.model';
+import jwt from "jsonwebtoken";
+import bcrypt from 'bcrypt';
+import dotenv from 'dotenv';
+import { describe } from "node:test";
+
+dotenv.config();
+
+const JWT_SECRET = process.env.JWT_SECRET || '';
+const server = new TestServer();
+server.app.use('/users', userRoutes);
+
+describe('User API Tests', () => {
+
+  let token: string;
+
+  beforeAll(async() => {
+    await server.start();
+    const hash = await bcrypt.hash('admin1234', 10);
+    const user = await User.create({
+      username: "asmin",
+      password: hash,
+      firstname: "testUser",
+      lastname: "testUser",
+      email: "testUser@aueb.gr"
+    });
+    const payload = {
+      username: user.username,
+      email: user.email,
+      roles: user.roles
+    }
+    token = jwt.sign(payload, JWT_SECRET,{expiresIn: '1h'} )
+  })
+
+  afterAll(async() => {await server.stop();});
+
+  test('GET /user -> returns list of users', async() => {
+    const res = await server.request.get('/users').set('Authorization', `Bearer ${token}`);
+    expect(res.status).toBe(201);
+    expect(Array.isArray(res.body)).toBe(true);
+  });
+})
+
